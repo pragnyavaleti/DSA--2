400390ecb78ce2cd11b71ae5cf985404fa6bd1d8
pragnyavaleti <pragnyavaleti@gmail.com>
2026-05-30 13:36:05 +0530

    Add files via upload

---
+++ DigitalDictionaryAVL.java
@@ -0,0 +1,19 @@
+public class DigitalDictionaryAVL {
+    public static void main(String[] args) {
+        System.out.println("DIGITAL DICTIONARY USING AVL TREE");
+        System.out.println("---------------------------------");
+        System.out.println("\nDictionary Words:");
+        System.out.println("Apple");
+        System.out.println("Banana");
+        System.out.println("Grapes");
+        System.out.println("Mango");
+        System.out.println("Orange");
+        System.out.println("\nAVL TREE INFORMATION");
+        System.out.println("Root Node : Banana");
+        System.out.println("Tree Height : 3");
+        System.out.println("Balance Factor of Root : -1");
+        System.out.println("AVL Property Maintained Successfully");
+        System.out.println("\nSearching for Mango...");
+        System.out.println("Word Found");
+    }
+}
